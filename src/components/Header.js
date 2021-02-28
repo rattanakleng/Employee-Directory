e82ae0f9b6dd9b8a74b9f1198cbb9dcbeaef472d
Rattanak Leng <rleng@live.com>
2021-02-28 15:06:34 -0800

    upload initial files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header () {
+
+    return (
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4 text-center">Employee Directory</h1>
+  </div>
+</div>
+    )
+}
+
+
---
+++ src/utils/API.js
@@ -0,0 +1,5 @@
+import axios from 'azios';
+
+export default {
+    
+}
