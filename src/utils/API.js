e3f7d9ad3d66ad16b559479f761edd5e575d64f6
Rattanak Leng <rleng@live.com>
2021-02-28 15:20:10 -0800

    add API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,10 @@
 import axios from 'azios';
 
 export default {
-    
-}
+    getRandom: function() {
+        return axios.get('https://randomuser.me/api/?results=50');
+    },
+    getGender: function(gender) {
+        return axios.get(`https://randomuser.me/api/?gender= ${gender}`)
+    }
+};
