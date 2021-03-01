d2fb39942b2797c10521288d22de9f0443027209
Rattanak Leng <rleng@live.com>
2021-03-01 11:18:01 -0800

    Update query link to get data from API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,50 @@
-export default function Body({users}) {
-    return (
+import React, { Component } from "react";
+import API from "../utils/API";
+
+class Body extends Component {
+
+    state = {
+        result:[],
+        search: ""
+      };
+
+    componentDidMount() {
+        this.searchEmployee(3);
         
-        <div className="container">
-            <div className="row">
+    }
 
-                {users.map(user =>
-                <div className="col-4">
-                        <div className="card" style={{ width: "18rem" }}>
+    searchEmployee = query => {
+        API.search(query)
+          .then(res => this.setState({ result: res.data.results }))
+            .catch(err => console.log(err));        
+       };
 
-                    <img src={user.picture.large} className="card-img-top" alt="Profile image" />
-
-                    <div className="card-body">
-                        <h5 className="card-title"> {user.name.first} {user.name.last} </h5>
-        
-                        <p className="card-text">Email: {user.email}</p>
-                    </div>
-                </div>
-                </div>
+    render() {
                 
-            )}
-
-            </div>
+        return (        
+            <div className="container">
+                <div className="row">    
+               {this.state.result.map(user =>
+                    <div className="col-4">
+                        <div className="card" style={{ width: "18rem" }}>
+    
+                            <img src={user.picture.large} className="card-img-top" alt="Profile img" />
+    
+                            <div className="card-body">
+                        
+                                <h5 className="card-title"> {user.name.first} {user.name.last} </h5>
             
-        </div>
+                                <p className="card-text">Email: {user.email}</p>
+                            </div>
+                        </div>
+                    </div>
                     
-            
-    )
+                )}
+    
+                </div>            
+            </div>        
+        )
+    }
 }
+
+export default Body;
