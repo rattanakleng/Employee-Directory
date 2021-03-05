8e544401ec08e062e28390df45b1ccc23a3256d4
Rattanak Leng <rleng@live.com>
2021-03-04 16:58:18 -0800

    Updates

--- src/components/EmployeeDetail.js
+++
@@ -1,7 +0,0 @@
-// export default function EmployeeDetail(props) {
-//     return (
-//         <div className="text-center">
-//             <img alt="Thumnail" className="img-fluid" src={props.src}
-//         </div>
-//     )
-// }
--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  getEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=50");
-  },
-
-  }
